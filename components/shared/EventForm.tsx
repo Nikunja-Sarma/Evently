f141e0a406264bbe9649653d500a521e784dfceb
Nikunja Sarma <xnikunja@gmail.com>
2023-12-27 22:31:41 +0530

    Event Page Updated

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -9,6 +9,10 @@ import { Input } from "@/components/ui/input"
 import { eventFormSchema } from "@/lib/validator"
 import { eventDefaultValues } from "@/constants"
 import Dropdown from "./Dropdown"
+import { Textarea } from "../ui/textarea"
+import { FileUploader } from "./FileUploader"
+import { useState } from "react"
+import Image from "next/image"
 
 
  
@@ -27,7 +31,7 @@ const EventForm = ({userId, type}: EventFormProps) => {
     resolver: zodResolver(eventFormSchema),
     defaultValues: initialValues
   })
- 
+ const [files, setFiles] = useState<File[]>([])
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof eventFormSchema>) {
     // Do something with the form values.
@@ -64,9 +68,65 @@ const EventForm = ({userId, type}: EventFormProps) => {
           </FormItem>
         )}
       />
-
       </div>
       
+      <div className="flex flex-col gap-5 md:flex-row">
+      <FormField
+        control={form.control}
+        name="description"
+        render={({ field }) => (
+          <FormItem className="w-full">
+            <FormControl className="h-72">
+              <Textarea
+ placeholder="description" {...field} className="textarea rounded-2xl" />
+            </FormControl>
+
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="description"
+        render={({ field }) => (
+          <FormItem className="w-full">
+            <FormControl className="h-72">
+              <FileUploader onFieldChange={field.onChange}
+              imageUrl={field.value}
+              setFiles={setFiles}
+              />
+            </FormControl>
+
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      </div>
+
+      <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+              control={form.control}
+              name="location"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                      <Image
+                        src="/assets/icons/location-grey.svg"
+                        alt="calendar"
+                        width={24}
+                        height={24}
+                      />
+
+                      <Input placeholder="Event location or Online" {...field} className="input-field" />
+                    </div>
+
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+        </div>
       <Button type="submit">Create Event</Button>
     </form>
   </Form>
