62b12bd06fe911af707a37429c4c83f4110d1d5d
Nikunja Sarma <xnikunja@gmail.com>
2023-12-27 21:28:56 +0530

    added event Title and Category section

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -3,26 +3,15 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
-
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
+import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { eventFormSchema } from "@/lib/validator"
+import { eventDefaultValues } from "@/constants"
+import Dropdown from "./Dropdown"
 
 
  
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
 
 type EventFormProps = {
     
@@ -32,39 +21,52 @@ type EventFormProps = {
 
 const EventForm = ({userId, type}: EventFormProps) => {
 
+  const initialValues = eventDefaultValues;
      // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
+  const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues: initialValues
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof eventFormSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values)
   }
   return (
     <Form {...form}>
-    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+    <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
+      <div className="flex flex-col gap-5 md:flex-row">
       <FormField
         control={form.control}
-        name="username"
+        name="title"
         render={({ field }) => (
-          <FormItem>
-            <FormLabel>Event Name</FormLabel>
+          <FormItem className="w-full">
             <FormControl>
-              <Input placeholder="Evently" {...field} />
+              <Input placeholder="Event Title" {...field} className="input-field" />
             </FormControl>
-            <FormDescription>
-              The name of the event!
-            </FormDescription>
+
             <FormMessage />
           </FormItem>
         )}
       />
+      <FormField
+        control={form.control}
+        name="categoryId"
+        render={({ field }) => (
+          <FormItem className="w-full">
+            <FormControl>
+              <Dropdown onChangeHandler={field.onChange} value={field.value} />
+            </FormControl>
+
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+
+      </div>
+      
       <Button type="submit">Create Event</Button>
     </form>
   </Form>
