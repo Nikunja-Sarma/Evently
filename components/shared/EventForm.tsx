9c3afde0a8b47a96b566d9770bce525212ac52cd
Nikunja Sarma <xnikunja@gmail.com>
2023-12-28 21:35:53 +0530

    updates

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -14,7 +14,7 @@ import { FileUploader } from "./FileUploader"
 import { useState } from "react"
 import Image from "next/image"
 import DatePicker from "react-datepicker";
-// import { useUploadThingfrom }  from '@/lib/uploadthing'
+
 
 import "react-datepicker/dist/react-datepicker.css";
 import { Checkbox } from "../ui/checkbox"
