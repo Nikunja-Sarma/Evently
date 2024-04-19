f80ee248be70e569816a5c184febd0d7a8c8b245
Nikunja-Sarma <154231867+Nikunja-Sarma@users.noreply.github.com>
2024-04-19 11:51:08 +0530

    Update order.actions.ts

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -36,6 +36,7 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
       mode: 'payment',
       success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
       cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
+      billing_address_collection: 'required',
     });
 
     redirect(session.url!)
